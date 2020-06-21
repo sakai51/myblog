e79ff8ccbc951a933c81a5554fd85402ef8d2cb8
“sakai51” <stakashi51@gmail.com>
2020-06-21 22:25:54 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# myblog
+ Laravelで作ったブログ
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <div class="container">
+    <h1>@yield('content')</h1>
+  </div>
+</body>
+</html>
